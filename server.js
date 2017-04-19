78df2be28da0c02509149b93aa1135040fc50e85
Mitch Moore <mamoore9369@gmail.com>
2017-04-18 21:49:15 -0400

    updated GET /todos to only pull user's todos

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function(req, res) {
 //GET /todos?completed=true&q=house
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
   var query = req.query;
-  var where = {};
+  var where = {
+    userId: req.user.get('id')
+  };
 
   if(query.hasOwnProperty('completed') && query.completed === 'true') {
     where.completed = true;
@@ -154,7 +156,7 @@ app.post('/users/login', function(req, res) {
   });
 });
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + '!');
   });
